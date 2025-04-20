09a98f44708ebb88d12ba207e91b1b4704ae9bf5
BarackAdida <adida.barack@student.moringaschool.com>
2025-04-20 19:35:36 +0300

    Intro to boolean

---
+++ bool.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    bool IsSick = false;
+    bool IsHealthy = true;
+    
+    cout << "IsSick:\n" <<IsSick <<"\n";
+    cout << "IsHealthy:\n" <<IsHealthy <<"\n";
+    return 0;
+}
