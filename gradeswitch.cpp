f50127f8763f12f6fe73e03b94cc7d1b362b7f71
BarackAdida <adida.barack@student.moringaschool.com>
2025-05-10 22:09:10 +0300

    C++ codes added

---
+++ gradeswitch.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char grade;
+
+    do {
+        cout << "\n\tGRADE MENU\n"
+             << "A - Excellent\n"
+             << "B - Very Good\n"
+             << "C - Good\n"
+             << "D - Fair\n"
+             << "E - Poor\n"
+             << "F - Fail\n"
+             << "X - Exit\n";
+             
+        cout << "Enter Grade or X to exit: ";
+        cin >> grade;
+
+        switch (toupper(grade)) {
+            case 'A':
+                cout << "Excellent\n";
+                break;
+            case 'B':
+                cout << "Very Good\n";
+                break;
+            case 'C':
+                cout << "Good\n";
+                break;
+            case 'D':
+                cout << "Fair\n";
+                break;
+            case 'E':
+                cout << "Poor\n";
+                break;
+            case 'F':
+                cout << "Fail\n";
+                break;
+            case 'X':
+                cout << "Exiting the program...\n";
+                break;
+            default:
+                cout << "Invalid Grade, try again.\n";
+        }
+
+    } while (toupper(grade) != 'X');
+
+    return 0;
+}
