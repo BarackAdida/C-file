e17fa84da60bab2e3f0faf543d414f1a7596b836
BarackAdida <adida.barack@student.moringaschool.com>
2025-05-14 11:35:34 +0300

    Power function added

---
+++ labs/power.cxx
@@ -0,0 +1,15 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+#include <cmath>
+
+void powfunction() {
+    long number = pow(2, 50);
+    long sol = number - 1;
+    cout << sol;
+}
+
+int main() {
+    powfunction();
+    return 0;
+}
