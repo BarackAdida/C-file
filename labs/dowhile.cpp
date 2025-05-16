0d6a41ac8f510e320218ba3087007eabcd78ce18
BarackAdida <adida.barack@student.moringaschool.com>
2025-05-16 12:35:12 +0300

    Do while loop

---
+++ labs/dowhile.cpp
@@ -0,0 +1,15 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 50;
+    do {
+        cout << num / 2 << "\n";
+        num = num + 2;
+    } while (num <= 400);
+    
+
+    return 0;
+}
+
