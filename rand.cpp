a591c86801577a642578497fc67e85b7d3c5e362
BarackAdida <adida.barack@student.moringaschool.com>
2025-05-18 21:16:14 +0300

    Rand and srand to generate random numbers

---
+++ rand.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void gene() {
+    srand(time(0));
+    // for (int num = 1; num <= 5; num ++) {
+        int randomnumber = rand() % 100;
+        cout << "Random number:\t" << randomnumber <<"\n";
+        // cout << "Random number " << num << ": " << randomnumber << "\n";
+    // }
+}
+
+int main () {
+    gene();
+}
