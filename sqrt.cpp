b57f85ba1a06f612d1f50c4e1926f71ea0a52298
BarackAdida <adida.barack@student.moringaschool.com>
2025-05-18 20:47:54 +0300

    square roots sqrt in cpp

---
+++ sqrt.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void calcsqrt() {
+    float number = sqrt(49);
+    float sum = 15 + number;
+    cout << sum << "\n";
+}
+
+int main () {
+    calcsqrt();
+}
